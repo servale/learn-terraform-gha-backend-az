888d5b798809d0252d3e6da02b2b303fbf3c86e2
servale <38044659+servale@users.noreply.github.com>
2026-01-08 12:46:29 -0500

    RG name changes

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "main" {
-  name     = "rg-${var.environment_name}-${var.application_name}"
+  name     = "rg-${var.application_name}-${var.environment_name}-state"
   location = var.primary_location
 
 
@@ -18,7 +18,7 @@ resource "random_string" "suffix" {
 }
 
 resource "azurerm_storage_account" "main" {
-  name                     = "st${var.environment_name}${random_string.suffix.result}"
+  name                     = "st${random_string.suffix.result}"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = var.storage_account_tier
@@ -46,5 +46,5 @@ resource "azurerm_storage_account" "main" {
 resource "azurerm_storage_container" "main" {
   name                  = var.blob_container_name
   storage_account_id    = azurerm_storage_account.main.id
-  container_access_type = lower(var.blob_access_type)
+  container_access_type = var.blob_access_type
 }
